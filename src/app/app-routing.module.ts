566ae6c4399f6ba2f63d06ad886b322dc76ab216
mehdy guettache <mehdy33852@gmail.com>
2023-04-21 13:57:59 +0200

    commit quete 4*

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserProfilComponent } from './user-profil/user-profil.component'
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'menu', component: MenuComponent },
+  { path: 'user-profil', component: UserProfilComponent },
+{ path: 'sign-up', component: SignUpComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    UserProfilComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
